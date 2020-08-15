9839dc20db4519ef9c2a90626cb1aa1edf1096b6
Agamiru <chidinnaemekannadi@gmail.com>
2020-08-15 19:11:38 +0100

    Implemented a somewhat manual table-to-json parser for bhphotovideo.com to be used by admins for adding product specs

---
+++ app_admin/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+html_data = """
+<table>
+  <tr>
+    <td>Card balance</td>
+    <td>$18.30</td>
+  </tr>
+  <tr>
+    <td>Card name</td>
+    <td>NAMEn</td>
+  </tr>
+  <tr>
+    <td>Account holder</td>
+    <td>NAME</td>
+  </tr>
+  <tr>
+    <td>Card number</td>
+    <td>1234</td>
+  </tr>
+  <tr>
+    <td>Status</td>
+    <td>Active</td>
+  </tr>
+</table>
+"""
+
