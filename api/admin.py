5ab9b46bb8418906734bc3934611f5cb55c18364
Agamiru <chidinnaemekannadi@gmail.com>
2020-09-14 14:16:52 +0100

    remodeling the models

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import (
     Product, MainFeatures, Brand, Category, SubCategory1,
-    SubCategory2, ModelName, Accessories, BoughtTogether
+    SubCategory2, ModelName, CategoryAccessories, CatAccessoryJoin   # Accessories, BoughtTogether
 )
 from django import forms
 from django.forms.widgets import TextInput
 from django.forms.fields import JSONField, JSONString
-from django.core.exceptions import ValidationError, ObjectDoesNotExist as doesnt_exist
+from django.core.exceptions import ValidationError, ObjectDoesNotExist as doesntExist
+from django.contrib import messages
 
 from app_admin.utils import BhphotovideoTableConverter
 import json
@@ -41,12 +42,12 @@ class InTheBoxField(JSONField):
         if not value:
             return None
         value = str(value)
-        # print(f"str_value: {str_value}")
-        if value.startswith("[") and value.endswith("]"):
-            print("Im a list\n")
+        if value.startswith("[") and value.endswith("]"):       # List checker
             return "in_database"
-        items = value.split("\n")
-        items_2 = value.split(",")
+        items = value.split("\n")       # For lists delimited by a new line character
+        items_2 = value.split(",")      # For lists delimited by a comma
+
+        # The longer of the two will be the final list
         items = [item.strip() for item in items] if len(items) > len(items_2) else [item.strip() for item in items_2]
 
         if isinstance(items, (list, dict, int, float, JSONString)):
@@ -69,23 +70,23 @@ class ProductForm(forms.ModelForm):
         specs = self.cleaned_data.get("specs")
         # specs.pop("Box Dimensions (LxWxH)")
         # specs.pop("Package Weight")
-        if not specs:
+        if not specs:       # Ideally, field validations should have checked for this already
             raise ValidationError("This field receives 'None' as value")
 
         id = self.instance.id
-        pd = self.cleaned_data.get("package_dimensions", None)
-        w = self.cleaned_data.get("weight")
-        print(f"Instance ID: {id}\n")
+        pd = self.cleaned_data.get("package_dimensions")  # Package Dimensions
+        w = self.cleaned_data.get("weight")     # Weight
+
         try:
-            obj = Product.objects.get(pk=id)
+            obj = Product.objects.get(pk=id)    # Try fetching existing object
             model_specs = obj.specs      # Existing Object specs
             model_itb = obj.in_the_box      # Existing Object in_the_box
             if specs == "in_database":
                 self.cleaned_data["specs"] = model_specs
 
-                model_specs_pd = model_specs.get("Box Dimensions (LxWxH)", None)
+                model_specs_pd = model_specs.get("Box Dimensions (LxWxH)")
                 model_specs_pd = model_specs_pd[0] if model_specs_pd else None
-                model_specs_w = model_specs.get("Package Weight", None)
+                model_specs_w = model_specs.get("Package Weight")
                 model_specs_w = model_specs_w[0] if model_specs_w else None
 
                 if not pd and model_specs_pd is not None:
@@ -96,23 +97,27 @@ class ProductForm(forms.ModelForm):
             if self.cleaned_data.get("in_the_box") == "in_database":
                 self.cleaned_data["in_the_box"] = model_itb
 
-        except doesnt_exist:    # For newly saved products
+        except doesntExist:    # For newly saved products
 
             try:
                 package_dims = specs["Box Dimensions (LxWxH)"]
-                # Confirm dims from spec are same as filled, else use dims from specs
+                # Use filled package dimensions if available, else take from specs
                 if not pd:
                     self.cleaned_data["package_dimensions"] = package_dims[0]
-            except KeyError as e:
-                self.add_error("package_dimensions", f"Specs has no {e}")
+            except KeyError:
+                pass
+                # Todo: Should display a message notifying the user there are no package_dimensions
+                # self.add_error("package_dimensions", f"Specs has no {e}")
 
             try:
                 weight = specs["Package Weight"]
                 # Confirm weight from spec are same as filled, else use weight from specs
                 if not w:
                     self.cleaned_data["weight"] = weight[0]
-            except KeyError as e:
-                self.add_error("weight", f"Specs has no {e}")
+            except KeyError:
+                pass
+                # Todo: Should display a message notifying the user there is no weight
+                # self.add_error("weight", f"Specs has no {e}")
 
         return self.cleaned_data
 
@@ -148,5 +153,7 @@ admin.site.register(Category)
 admin.site.register(SubCategory1)
 admin.site.register(SubCategory2)
 admin.site.register(ModelName)
-admin.site.register(Accessories)
-admin.site.register(BoughtTogether)
+admin.site.register(CategoryAccessories)
+admin.site.register(CatAccessoryJoin)
+# admin.site.register(Accessories)
+# admin.site.register(BoughtTogether)
