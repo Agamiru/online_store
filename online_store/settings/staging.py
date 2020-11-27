fd834ebf10b27727da892fae6945b0dfbf52bfab
Agamiru <chidinnaemekannadi@gmail.com>
2020-11-27 10:01:54 +0100

    check

--- online_store/settings/staging.py
+++ online_store/settings/staging.py
@@ -8,7 +8,7 @@ from dj_database_url import parse as dburl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
