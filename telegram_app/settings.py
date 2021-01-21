f68d123b04b013e5f75ffff319fd1536f82b3954
Agamiru <chidinnaemekannadi@gmail.com>
2021-01-21 10:23:05 +0100

    check

--- telegram_app/settings.py
+++ telegram_app/settings.py
@@ -4,7 +4,8 @@ from decouple import config
 import telegram
 
 BOT_TOKEN = config("BOT_TOKEN")
-if os.environ.get("DJANGO_SETTINGS_MODULE") == "online_store.settings.development":
+if os.environ.get("DJANGO_SETTINGS_MODULE") == "online_store.settings.development" and \
+        os.environ.get('GITHUB_WORKFLOW') is None:
     webhook_url = f"{config('NGROK_BASE_URL')}{config('BOT_TOKEN')}/"
 else:
     webhook_url = f"{config('BASE_URL')}{config('BOT_TOKEN')}/"
